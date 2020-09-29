b93d8061e2d4de9b67da064da047eb4c3cb54084
Brian C. Milco <bcmilco@gmail.com>
2020-09-28 17:44:33 -0700

    Add API class and front end for loading Bible passages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ class App extends Component {
   state = {
     loading: true,
     bibles: [],
-    languages: []
+    books: [],
+    chapters: [],
+    languages: [],
+    language: 'eng',
+    bible: '',
+    chapter: '',
+    content: []
   };
 
   async componentDidMount() {
@@ -33,13 +39,51 @@ class App extends Component {
   }
 
   async languageChanged( event ) {
-    const response = await fetch( `http://localhost:8081/bibles?language=${event.target.value}` );
+    let value = event.target.value;
+    const response = await fetch( `http://localhost:8081/bibles?language=${value}` );
     let data = await response.json();
 
     this.setState( {
       loading: false,
-      bibles: data.data
+      bibles: data.data,
+      language: value
     } );
+  }
+
+  async bibleChanged( event ) {
+    let value = event.target.value;
+    const response = await fetch( `http://localhost:8081/books?bible=${value}` );
+      let data = await response.json();
+
+      this.setState( {
+        loading: false,
+        books: data.data,
+        bible: value
+      } );
+  }
+
+  async bookChanged( event ) {
+    let value = event.target.value;
+    const response = await fetch( `http://localhost:8081/chapters?bible=${this.state.bible}&book=${value}` );
+      let data = await response.json();
+
+      this.setState( {
+        loading: false,
+        chapters: data.data,
+        book: value
+      } );
+  }
+
+  async chapterChanged( event ) {
+    let value = event.target.value;
+    const response = await fetch( `http://localhost:8081/text?bible=${this.state.bible}&chapter=${value}` );
+      let data = await response.json();
+
+      this.setState( {
+        loading: false,
+        content: data.data,
+        chapter: value
+      } );
   }
 
   render() {
@@ -52,20 +96,43 @@ class App extends Component {
         </header>
         <label>
           Languages:
-          <select id='languages' onChange={( event ) => this.languageChanged(event)}>
+          <select id='languages' onChange={ ( event ) => this.languageChanged( event ) }>
           {this.state.languages.map( ( language ) => (
             <option key={language.id} value={language.id}>{language.name}</option>
           ) ) }
           </select>
         </label>
+        <br />
         <label>
           Bible:
-          <select id='bible'>
+          <select id='bible' onChange={ ( event ) => this.bibleChanged( event ) }>
           {this.state.bibles.map( ( bible ) => (
             <option key={bible.id} value={bible.id}>{bible.name}</option>
           ) ) }
           </select>
         </label>
+        <br />
+        <label>
+          Books:
+          <select id='books' onChange={ ( event ) => this.bookChanged( event ) }>
+          {this.state.books.map( ( book ) => (
+            <option key={book.id} value={book.id}>{book.name}</option>
+          ) ) }
+          </select>
+        </label>
+        <br />
+        <label>
+          Chapters:
+          <select id='chapters' onChange={ ( event ) => this.chapterChanged( event ) }>
+          {this.state.chapters.map( ( chapter ) => (
+            <option key={chapter.id} value={chapter.id}>{chapter.number}</option>
+          ) ) }
+          </select>
+        </label>
+        <div dangerouslySetInnerHTML={{__html: this.state.content.content }} />
+        <div className='brp-bible-copy-right'>
+          {this.state.content.copyright}
+        </div>
       </div>
     );
   }
--- src/backend/server.js
+++ src/backend/server.js
@@ -16,7 +16,28 @@ let mongo_options = {
 };
 
 /*
- * Get all the pages for a given user and notebook
+ * Get all the languages from the server
+ */
+app.get( '/languages/', ( req, res ) => {
+  MongoClient.connect( db_uri, mongo_options, (err, client) => {
+    if(err) throw err;
+
+    let criteria = {
+      user_id: '',
+      notebook: '',
+    };
+
+    let cursor = client.db('bibleplans').collection( 'languages-temp' ).find();
+
+    cursor.toArray( ( err, result ) => {
+      if (err) throw err;
+      res.send( result );
+    } );
+  } );
+} );
+
+/*
+ * Get all the Bibles for a given language
  */
 app.get( '/bibles/', ( req, res ) => {
   let langs = [];
@@ -39,23 +60,85 @@ app.get( '/bibles/', ( req, res ) => {
 } );
 
 /*
- * Get all the languages from the server
+ * Get all the books for the given Bible
  */
-app.get( '/languages/', ( req, res ) => {
-  MongoClient.connect( db_uri, mongo_options, (err, client) => {
-    if(err) throw err;
+app.get( '/books/', ( req, res ) => {
+  let langs = [];
+  let query = `${config.bible_api_url}/bibles/`;
 
-    let criteria = {
-      user_id: '',
-      notebook: '',
-    };
+  if( req.query.bible ) {
+    query += `${req.query.bible}/books`;
+  }
+  if( req.query.language ) {
+    query += `?language=${req.query.language}`;
+  }
 
-    let cursor = client.db('bibleplans').collection( 'languages-temp' ).find();
+  request( {
+    uri: query,
+    json: true,
+    headers: {
+      'api-key': `${config.bible_api_key}`,
+    }
+  }, (err, r, body) => {
+    if (err) return console.log(err);
 
-    cursor.toArray( ( err, result ) => {
-      if (err) throw err;
-      res.send( result );
-    } );
+    res.send( body );
+  } );
+} );
+
+/*
+ * Get all the chapters for a given book of the Bible
+ */
+app.get( '/chapters/', ( req, res ) => {
+  let langs = [];
+  let query = `${config.bible_api_url}/bibles/`;
+
+  if( req.query.bible ) {
+    query += `${req.query.bible}/books/`;
+  }
+  if( req.query.book ) {
+    query += `${req.query.book}/chapters`;
+  }
+
+  request( {
+    uri: query,
+    json: true,
+    headers: {
+      'api-key': `${config.bible_api_key}`,
+    }
+  }, (err, r, body) => {
+    if (err) return console.log(err);
+
+    console.log( body );
+    res.send( body );
+  } );
+} );
+
+/*
+ * Get the text for a given chapter of a book of the Bible
+ */
+app.get( '/text/', ( req, res ) => {
+  let langs = [];
+  let query = `${config.bible_api_url}`;
+
+  if( req.query.bible ) {
+    query += `/bibles/${req.query.bible}`;
+  }
+  if( req.query.chapter ) {
+    query += `/chapters/${req.query.chapter}`;
+  }
+console.log( query );
+  request( {
+    uri: query,
+    json: true,
+    headers: {
+      'api-key': `${config.bible_api_key}`,
+    }
+  }, (err, r, body) => {
+    if (err) return console.log(err);
+
+    console.log( body );
+    res.send( body );
   } );
 } );
 
